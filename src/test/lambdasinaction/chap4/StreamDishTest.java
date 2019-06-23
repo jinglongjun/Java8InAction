23473266ae45812ceae924d3fbb16140f2feab46
jinglongjun <jinglongjun@raycloud.com>
2019-06-23 14:09:10 +0800

    add IllegalStateException test

--- src/test/lambdasinaction/chap4/StreamDishTest.java
+++ src/test/lambdasinaction/chap4/StreamDishTest.java
@@ -5,11 +5,20 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDishTest {
 
     List<Dish> menu = Arrays.asList(
-            new Dish("pork", false, 800, Dish.Type.MEAT), new Dish("beef", false, 700, Dish.Type.MEAT), new Dish("chicken", false, 400, Dish.Type.MEAT), new Dish("french fries", true, 530, Dish.Type.OTHER), new Dish("rice", true, 350, Dish.Type.OTHER), new Dish("season fruit", true, 120, Dish.Type.OTHER), new Dish("pizza", true, 550, Dish.Type.OTHER), new Dish("prawns", false, 300, Dish.Type.FISH), new Dish("salmon", false, 450, Dish.Type.FISH));
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 300, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH));
 
     @Test
     public void test01() {
@@ -17,9 +26,17 @@ public class StreamDishTest {
         List<String> threeHighCaloricDishName =
                 menu.stream().filter(d -> d.getCalories() > 300)
                         .map(Dish::getName)
-                        .limit(3)
+                        .limit(3)//limit 只是简单的截取，不会做任何排序
                         .collect(Collectors.toList());
         System.out.println(threeHighCaloricDishName);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test02() {
+        List<String> title = Arrays.asList("Java8", "In", "Action");
+        Stream<String> s = title.stream();
+        s.forEach(System.out::println);
+        s.forEach(System.out::println);
     }
 
 
